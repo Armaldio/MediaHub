2190a5cc8a7efe1d258f2a03285e00aa65ed137d
Quentin Goinaud <quentin.goinaud@gmail.com>
2025-11-14 14:50:35 +0100

    -

--- src/main.ts
+++ src/main.ts
@@ -19,18 +19,16 @@ SafeArea.enable({
 });
 
 const configure = async () => {
-  const platform = Capacitor.getPlatform();
-  console.log("platform", platform);
-
-  if (platform === "ios" || platform === "android") {
-    await Purchases.setLogLevel({ level: LOG_LEVEL.DEBUG });
-  }
-
-  if (platform === "ios") {
-    await Purchases.configure({ apiKey: "test_mgsOnjKuwgqXGUecJWwvTrvOfGC" });
-  } else if (platform === "android") {
-    await Purchases.configure({ apiKey: "test_mgsOnjKuwgqXGUecJWwvTrvOfGC" });
-  }
+  // const platform = Capacitor.getPlatform();
+  // console.log("platform", platform);
+  // if (platform === "ios" || platform === "android") {
+  //   await Purchases.setLogLevel({ level: LOG_LEVEL.DEBUG });
+  // }
+  // if (platform === "ios") {
+  //   await Purchases.configure({ apiKey: "test_mgsOnjKuwgqXGUecJWwvTrvOfGC" });
+  // } else if (platform === "android") {
+  //   await Purchases.configure({ apiKey: "test_mgsOnjKuwgqXGUecJWwvTrvOfGC" });
+  // }
 };
 
 const app = createApp(App);
